8b01fdfb168f5dd9660eace22e9e1c77bc191e16
Ivan Kayzer <ivan.kayzer@outlook.com>
2019-08-23 17:01:15 +0200

    Царский подгон

--- routes/botman.php
+++ routes/botman.php
@@ -5,21 +5,21 @@ use BotMan\Drivers\Telegram\TelegramDriver;
 
 $botman = resolve('botman');
 
-$urlTok = config('services.vk.url') . config('services.vk.token');
-$response = json_decode(file_get_contents($urlTok));
-$factory = PostFactory::make($response);
-$message = $factory->getMessage();
-
-$botman->hears('/help', function ($bot) use ($message) {
-    $bot->reply($message);
+$botman->hears('1', function ($bot) use ($botman) {
+    \App\Jobs\HelloJob::dispatch();
 });
 
-$botman->hears('/start', function ($bot) use ($botman, $message) {
-    $botman->say($message, '1566560716001', TelegramDriver::class);
+$botman->hears('start', function ($bot) {
+    $bot->reply(
+        sprintf(
+            '<a href="https://oauth.vk.com/authorize?client_id=%s&display=page&redirect_uri=http://botman.test/success&scope=%s&response_type=token&v=5.52">Authorize</a>',
+            '7109046',
+            'offline'
+        ), ['parse_mode' => 'HTML']
+    );
 });
 
 
 
 
-
-
+https://oauth.vk.com/authorize?client_id=7108999&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=offline&response_type=token&v=5.52
