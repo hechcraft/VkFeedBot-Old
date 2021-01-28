ce7b220e58256d3311dc736c9d00dbfe62596135
Den Yatsenko <Hechcraft@gmail.com>
2021-01-28 19:48:13 +0300

    edit .env.example and bug fix

--- app/Services/Hasher.php
+++ app/Services/Hasher.php
@@ -7,6 +7,9 @@ class Hasher
     public static function makeFromPost($post)
     {
         $postId = $post->post_id ?? $post->text;
+        if (!$postId){
+            $postId = serialize($post);
+        }
         return md5($post->source_id . $postId);
     }
 }
