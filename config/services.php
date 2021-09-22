171199cf0624d93d9a707133cb6d1eb7b2f169ae
Ivan Kayzer <ivan.kayzer@outlook.com>
2021-09-22 21:45:03 +0200

    bump vk api version

--- config/services.php
+++ config/services.php
@@ -16,7 +16,7 @@ return [
 
     'vk' => [
         'token' => env('VK_TOKEN'),
-        'url' => 'https://api.vk.com/method/newsfeed.get?&filters=posts&count=postsAmount&return_banned=0&v=5.52&access_token=',
+        'url' => 'https://api.vk.com/method/newsfeed.get?&filters=posts&count=postsAmount&return_banned=0&v=5.81&access_token=',
     ],
 
     'mailgun' => [
